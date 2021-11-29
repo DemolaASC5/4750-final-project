3025a85be97e2c945275c351ddfebcb2ac953f5b
Demola <dko22@cornell.edu>
2021-11-29 17:42:29 -0500

    Rough addition of data

---
+++ project backup 1.jl
@@ -0,0 +1,20 @@
+### A Pluto.jl notebook ###
+# v0.17.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 06f79570-4636-11ec-1ab2-f36e6b5586f3
+students = [(name = "Anna Cliche", cornell_id = "amc527"),
+			(name = "Cormac Mahoney", cornell_id = "cam495"),
+			(name = "Demola Ogunnaike", cornell_id = "dko22")
+		   ]
+
+
+# ╔═╡ 7c00440f-18fb-45c8-ad6f-6db855afbea2
+# test
+# test2
+# test 3
+
+# ╔═╡ Cell order:
+# ╠═06f79570-4636-11ec-1ab2-f36e6b5586f3
